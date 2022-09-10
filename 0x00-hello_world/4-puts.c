cd6842ebaefe3789f7626cc1f5321ebd387c4950
Akbar3A <icerockcage@gmail.com>
2022-09-09 19:58:46 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash 
 #include <stdio.h>
 /**
  * Description ;main -Prints out a string to stdout.
- * Return: 0 if successful 
+ * Return: 0 if successful
 */
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle");
-return (0);
+        puts("\"Programming is like building a multiligual puzzle");
+        return (0);
 }
+
