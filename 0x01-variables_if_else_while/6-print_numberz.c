bcc768639091f50defb94f0846dae428944d767e
Akbar3A <icerockcage@gmail.com>
2022-09-10 20:57:46 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main -  prints all single digit numbers of base ten starting from zero
  *
  * Description: using the main function
- * This program " prints all single digit numbers of base 10 starts from 0 then a newline
+ * This program " prints all single digit numbers of base 10 starts from 0
  * Return: 0
  */
 int main(void)
